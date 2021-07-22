e9c96561e3c0be8cd81ae86e724adcc6b400b616
ZhaomingChen <87289369+coder816@users.noreply.github.com>
2021-07-22 10:50:21 +0800

    init

--- src/components/content/MainTabBar.vue
+++ src/components/content/MainTabBar.vue
@@ -1,31 +1,31 @@
 <template>
   <tab-bar>
     <TabBarItem path="/home" activeColor="#f86401">
-      <img slot="item-icon" src="../../assets/img/tabbar/home.svg" alt="">
-      <img slot="item-icon-active" src="../../assets/img/tabbar/home-active.svg" alt="">
-      <div slot="item-text">首页</div>
+      <template v-slot:item-icon></template><img src="~assets/img/tabbar/home.svg" alt="">
+      <template v-slot:item-icon-active></template><img src="~assets/img/tabbar/home-active.svg" alt="">
+      <template v-slot:item-text><div>首页</div></template>
     </TabBarItem>
     <TabBarItem path="/category" activeColor="#f86401">
-      <img slot="item-icon" src="../../assets/img/tabbar/category.svg" alt="">
-      <img slot="item-icon-active" src="../../assets/img/tabbar/category-active.svg" alt="">
-      <div slot="item-text">分类</div>
+      <template v-slot:item-icon></template><img src="~assets/img/tabbar/category.svg" alt="">
+      <template v-slot:item-icon-active></template><img src="~assets/img/tabbar/category-active.svg" alt="">
+      <template v-slot:item-text><div>分类</div></template>
     </TabBarItem>
     <TabBarItem path="/shopcart" activeColor="#f86401">
-      <img slot="item-icon" src="../../assets/img/tabbar/shopcart.svg" alt="">
-      <img slot="item-icon-active" src="../../assets/img/tabbar/shopcart-active.svg" alt="">
-      <div slot="item-text">购物车</div>
+      <template v-slot:item-icon></template><img src="~assets/img/tabbar/shopcart.svg" alt="">
+      <template v-slot:item-icon-active></template><img src="~assets/img/tabbar/shopcart-active.svg" alt="">
+      <template v-slot:item-text><div>购物车</div></template>
     </TabBarItem>
     <TabBarItem path="/profile" activeColor="#f86401">
-      <img slot="item-icon" src="../../assets/img/tabbar/profile.svg" alt="">
-      <img slot="item-icon-active" src="../../assets/img/tabbar/profile-active.svg" alt="">
-      <div slot="item-text">我的</div>
+      <template v-slot:item-icon></template><img src="~assets/img/tabbar/profile.svg" alt="">
+      <template v-slot:item-icon-active></template><img src="~assets/img/tabbar/profile-active.svg" alt="">
+      <template v-slot:item-text><div>我的</div></template>
     </TabBarItem>
   </tab-bar>
 </template>
 
 <script>
-import TabBar from "@components/common/tabbar/TabBar";
-import TabBarItem from "@components/common/tabbar/TabBarItem";
+import TabBar from "@/components/common/tabbar/TabBar";
+import TabBarItem from "@/components/common/tabbar/TabBarItem";
 
 export default {
   name: "MainTabBar",
