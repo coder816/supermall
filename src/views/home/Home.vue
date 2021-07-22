bfa973c34ea9f63e1864bd289f16ba62700f2d08
ZhaomingChen <87289369+coder816@users.noreply.github.com>
2021-07-22 18:22:09 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main.js
    #	src/router/index.js

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,21 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar>
+      <template v-slot:nav-left></template>
+      <template v-slot:nav-center><h2>首页</h2></template>
+      <template v-slot:nav-right></template>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar
+  }
 }
 </script>
 
